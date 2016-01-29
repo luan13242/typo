6d0be697f30fcad75b9746b8016c46b8a0be6fdf
luan1324 <luan1324@gmail.com>
2016-01-29 01:54:28 +0000

    shit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -27,7 +27,8 @@ class ArticlesController < ContentController
       @articles = Article.published.page(params[:page]).per(@limit)
     end
 
-    @page_title = index_title
+    #@page_title = index_title
+    @page_title = "nasty stuff!!!!!"
     @description = index_description
     @keywords = (this_blog.meta_keywords.empty?) ? "" : this_blog.meta_keywords
 
